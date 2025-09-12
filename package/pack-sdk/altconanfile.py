e0f54dfdfc1d96835ba3d7bc0d5a555dd1cdfc0d
ALBIN SANTHOSH <albin.santhosh@ignitarium.com>
2025-09-12 12:42:02 +0530

    chores : Update License Header
    Updated the License Header with Company name and URL

--- package/pack-sdk/altconanfile.py
+++ package/pack-sdk/altconanfile.py
@@ -1,18 +1,15 @@
-/* 
- * SPDX-License-Identifier: MIT-0 OR Apache-2.0
- * 
- * Copyright (c) 2025 Ignitarium Technologies & Solutions, Ltd.
- * 
- * https://ignitarium.com/
- *
- * This source code is licensed under the MIT No Attribution License (MIT-0)
- * or the Apache License, Version 2.0, at your option.
- * You may not use this file except in compliance with one of these licenses.
- *
- * MIT-0 License: https://spdx.org/licenses/MIT-0.html
- * Apache-2.0 License: https://www.apache.org/licenses/LICENSE-2.0
- *
- */
+ # SPDX-License-Identifier: MIT-0 OR Apache-2.0
+ #
+ # Copyright (c) 2025 Ignitarium Technology Solutions Pvt Ltd
+ #
+ # https://ignitarium.com/
+ #
+ # This source code is licensed under the MIT No Attribution License (MIT-0)
+ # or the Apache License, Version 2.0, at your option.
+ # You may not use this file except in compliance with one of these licenses.
+ #
+ # MIT-0 License: https://spdx.org/licenses/MIT-0.html
+ # Apache-2.0 License: https://www.apache.org/licenses/LICENSE-2.0
 
 from conan import ConanFile
 from conan.tools.files import copy
